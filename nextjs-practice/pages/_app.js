ef2bcf6814ac1b14ca2bb54b0cada70e03f7126f
ezdar2743@naver.com <ezdar2743@gmail.com>
2022-03-26 15:33:13 +0900

    Layout

--- nextjs-practice/pages/_app.js
+++ nextjs-practice/pages/_app.js
@@ -1,10 +1,10 @@
+import Layout from "../components/Layout";
 import Nav from "../components/Nav";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <Nav></Nav>
-      <Component {...pageProps}>g</Component>
-    </>
+    <Layout>
+      <Component />
+    </Layout>
   );
 }
